66cd16c3693619e6f4e10e1dfe86118968a86382
psalms4 <psalms.mutiso@strathmore.edu>
2025-12-23 09:28:06 +0100

    Assignment is done

--- build_pipeline.py
+++ build_pipeline.py
@@ -1,4 +1,34 @@
+#Write a function:
+#build_pipeline(operation_names)
+#where operation_names is a list of strings.
+#The function must return a new function that applies a sequence of operations to a single input value,in the given order.
+#Each string in operation_names represents an operation
+#If an unknown operation name is encountered, an error must be raised.
+#Calling the returned function should apply all operations sequentially and return the final result.
+
 def build_pipeline(operation_names):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    operations = {
+        "add": lambda x: x + 4,
+        "sub": lambda x: x - 2,
+        "double": lambda x: 2 * x,
+        "triple": lambda x: 3 * x,
+        "square": lambda x : x ** 2,
+        
+        }
+    pipeline_ops = []
+    
+    for name in operation_names:
+        try:
+            pipeline_ops.append(operations[name])
+        except KeyError:
+            raise KeyError(f"Unknown operations: {name}")
+            
+    def pipeline(x):
+        for op in pipeline_ops:
+            x = op(x)
+        return x
+    return pipeline
+    
+   
+        
+    
--- safe_call.py
+++ safe_call.py
@@ -1,4 +1,35 @@
+#Write a function:
+#safe_call(func, a, b)
+#that attempts to call the function func with the given arguments.
+#The function must return a tuple of three elements:
+#- a boolean indicating whether the call was successful,
+#- the result of the function call (or None if an error occurred),
+#- the name of the exception as a string (or None if no error occurred).
+#If the function call raises one of the following exceptions:
+#- ZeroDivisionError
+#- TypeError
+#- ValueError
+#- IndexError
+#- KeyError
+#the function must not crash, but instead return information about the error.
+#Any other exception types should not be handled.
+
 def safe_call(func, a, b):
-    ### Replace with your own code (begin) ###
-    pass
-    ### Replace with your own code (end)   ###
+    try:
+        result = func(a,b)
+        return (True, result, None)
+    
+    except (ZeroDivisionError, TypeError, ValueError, IndexError, KeyError) as error:
+        return (False, None, type(error).__name__)
+    
+    
+def add(x,y):
+    return x + y
+
+safe_call(add, 4 ,5)
+print(safe_call(add, 4, 5))
+#(True, 9, None)
+safe_call(add, 6 , "six seven")
+print(safe_call(add, 6 ,"six seven"))
+#(False, None, 'TypeError')
+   
